9f8b39ed1ddd9df5dfe3b8fa90ef77c045a23c5d
tony940125 <tonytsai0125@gmail.com>
2024-07-02 11:55:52 +0800

    Add files via upload

---
+++ TKU-FCP-412110123/simple.java/simple.java
@@ -0,0 +1,8 @@
+
+public class simple {
+
+    public static void main(String args[]) {
+        System.out.println("Hello java");
+        System.out.println("0702");
+    }
+}
---
+++ ch2_1/ch2_1.java
@@ -0,0 +1,7 @@
+public class ch2_1 {//定義類別 ch2_1
+    public static void main(String[] args) {//函數，主程式開始
+        int num1=2;//宣告整數rum
+        System.out.println("cats are running");//印出字串及變數內容
+
+    }
+}
